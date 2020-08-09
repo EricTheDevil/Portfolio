95af9bc114ce3f3d66cd52bdf63b053978b16a99
Eric <EricTheDevil@hotmail.com>
2020-08-09 18:12:28 +0300

    added backgrounds and multi page view

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App"
 
+
+
+const myfirstelement = <h1>Hello React!</h1>
+/*
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+  <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
+  registerServiceWorker();
+  */
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
