265f424faba95def72c39ab93d6abad922355947
Eric <EricTheDevil@hotmail.com>
2020-08-10 12:18:28 +0300

    fixing case sensitive issues

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from "./app"
+
+
+
+const myfirstelement = <h1>Hello React!</h1>
+/*
+ReactDOM.render(
+    <BrowserRouter>
+  <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
+  registerServiceWorker();
+  */
+
+ReactDOM.render(<App />, document.getElementById('root'));
