33c48bc57de0b2e4092cf02bbd81df868b706d4e
Eric <EricTheDevil@hotmail.com>
2020-08-10 12:31:32 +0300

    case sensitive fix in the import from header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropDown';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
